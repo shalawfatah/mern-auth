8c7ef0b36975bbcb645fd4f6ad8428891d55b232
Shalaw Fatah <shalaw.fatah@gmail.com>
2021-04-22 09:49:36 -0700

    initial commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('App is running on port ' + process.env.PORT)
+})
